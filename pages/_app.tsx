36dad33ae180a359f66d9d8bcb28ec7021aa6f80
Nick <nil021@ucsd.edu>
2021-04-26 22:53:10 -0700

    fixed index/_app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import NavBar from './components/NavBar';
 import HeaderContent from './components/HeaderContent';
 import CardContainer from './components/CardContainer';
 
-
 export default class App extends React.Component {
     render() {
         return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './_app';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+export default App;
